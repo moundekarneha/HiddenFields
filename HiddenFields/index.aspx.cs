e1cb636134307a1450d760341d06a4b8ca90eab7
HP <HP@DESKTOP-G4J7E8F>
2022-07-21 17:41:06 +0530

    Add project files.

---
+++ HiddenFields/index.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HiddenFields
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            HiddenField1.Value = TextBox1.Text;
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Text = HiddenField1.Value;
+        }
+    }
+}
